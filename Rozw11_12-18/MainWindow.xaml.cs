cc0c718853b616df20aa17b8161852ff9b408225
Piotrek <@>
2017-12-30 14:26:05 +0100

    przeslanie plikow

---
+++ Rozw11_12-18/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rozw11_12_18
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Przycisk_Click(object sender, RoutedEventArgs e)
+        {
+            double liczba1 = Convert.ToDouble(Liczba1.Text);
+            double liczba2 = Convert.ToDouble(Liczba2.Text);
+            double wynik = liczba1 + liczba2;
+            Wynik.Content = wynik.ToString();
+        }
+    }
+}
